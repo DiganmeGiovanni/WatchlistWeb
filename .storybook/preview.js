06377b3e7bcc52c8a6c331c078df529039513281
Giovanni Aguirre <gaguirre@skytouchtechnology.com>
2020-09-06 00:43:25 -0500

    :rocket: Add bootstrap viewports to preview iframe

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,4 +4,43 @@ import '../src/sass/watchlist.scss'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  viewport: {
+    viewports: {
+      extraSmall: {
+        name: 'Extra Small (320px)',
+        styles: {
+          width: '320px',
+          height: '1080px',
+        }
+      },
+      small: {
+        name: 'Small (576)',
+        styles: {
+          width: '576px',
+          height: '1080px',
+        }
+      },
+      medium: {
+        name: 'Medium (768)',
+        styles: {
+          width: '768px',
+          height: '1080px',
+        }
+      },
+      large: {
+        name: 'Large (992)',
+        styles: {
+          width: '921px',
+          height: '1080px',
+        }
+      },
+      extraLarge: {
+        name: 'Extra large (1200)',
+        styles: {
+          width: '1200px',
+          height: '1080px'
+        }
+      }
+    }
+  }
 }
